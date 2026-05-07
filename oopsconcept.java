b81ff367d94b4b41b0d7431df53eba54d6893f55
Tanuja612 <tanutanuja57@gmail.com>
2026-05-07 12:24:38 +0530

    Add files via upload

---
+++ oopsconcept.java
@@ -0,0 +1,17 @@
+package day7;
+
+public class oopsconcept {
+	//non static variables
+	int age=20;
+	String name=" ";
+	
+	public static void main(String[] args)
+	{
+		oopsconcept s=new oopsconcept();
+		
+		System.out.println(s.age);
+		
+		System.out.println(s.name);
+	}
+
+}
